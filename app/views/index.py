3f07be5a2198cbf943fe41c2f2864c108b6948a0
alexop89056 <19802004v@mail.ru>
2024-05-05 15:30:09 +0400

    Пофиксил баг с css

--- app/views/index.py
+++ app/views/index.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 
 
+@never_cache
 def index(request):
     return render(request, 'index.html')
 
 
+@never_cache
 def catalog(request):
     return render(request, 'catalog.html')
