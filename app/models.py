1386153d3844bb20ce1c4d2627b51e421303b42b
alexop89056 <19802004v@mail.ru>
2024-06-28 15:01:02 +0400

    Ввел базовый текст

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Work(models.Model):
+    short_title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    main_img = models.ImageField(upload_to="work")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
